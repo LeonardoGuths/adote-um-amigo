2ea08ce59d0e23de3de6ced087f0549b40dce825
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2022-03-30 11:44:38 -0300

    feat: how it works part added, with magic scroll script feature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react"
-
+import "../styles/footer.css"
+import GatsbySVG from "../assets/gatsby.svg"
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="page-footer">
+      <div className="footer-text">
+        <p>
+          {new Date().getFullYear()} &copy; Todos os direitos reservAUdos. Feito
+          com{" "}
+        </p>
+        <a href="https://www.gatsbyjs.com/">
+          Gatsby <GatsbySVG />
+        </a>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,29 @@ import Layout from "../components/Layout"
 import "../styles/home.css"
 import { StaticImage } from "gatsby-plugin-image"
 import Button from "../components/Button"
+import { useEffect } from "react"
+import AboutText from "../components/AboutText"
+
 export default function Home() {
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      let value = window.scrollY
+      document.getElementById("catsndogs2").style.left = value * 0.75 + "px"
+      document.getElementById("catsndogs3").style.left = value * -0.75 + "px"
+      document.getElementById("welcome_title").style.marginTop =
+        value * 0.3 + "px"
+      document.getElementById("middle_button").style.marginTop =
+        value * 0.45 + "px"
+      document.querySelector("header").style.marginTop = value * 0.75 + "px"
+    })
+  })
   return (
     <Layout>
       <div className="home-page">
         <section className="start-section">
           <div className="welcome-text">
-            <h1>Adote um Amigo</h1>
-            <p>
+            <h1 id="welcome_title">Adote um Amigo</h1>
+            <p id="welcome_text">
               Bem-vindo! Que bom ter você por aqui! Esta página foi criada com o
               intuito de facilitar o encontro de nossos amados pets com seus
               futuros tutores!
@@ -41,6 +56,7 @@ export default function Home() {
             text="Conhecer"
           />
         </section>
+        <AboutText />
       </div>
     </Layout>
   )

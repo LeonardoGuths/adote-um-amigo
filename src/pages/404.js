3fcfc3efc7399bfd951db60328a9591c5163ec1f
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2022-05-10 11:39:07 -0300

    feat: 404 page boilerplate added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,29 @@
 import React from "react"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import "../styles/404.css"
 
 const Error = () => {
-  return <div>404 error page</div>
+  return (
+    <Layout>
+      <div className="error-page">
+        <div className="error-text">
+          <h2>Ops... Parece que não encontramos a página que você queria :(</h2>
+        </div>
+        <div className="error-image">
+          <StaticImage
+            src="../assets/images/catsndogs1.png"
+            alt="cats and dogs"
+            placeholder="blurred"
+            layout="constrained"
+            className="images1"
+            id="catsndogs1"
+            width={1920}
+          />
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Error
